77833d3791ae3c01da0de53ecaeb3c57f071323c
manumano <manojkumar207136@gmail.com>
2022-02-15 07:28:33 -0800

    m

--- src/org/com/test.java
+++ src/org/com/test.java
@@ -6,4 +6,15 @@ System.out.println("india");
 }
 private void china() {
 System.out.println("china");}
+
+private void japan() {
+System.out.println("japan");
+}
+public static void main(String[] args) {
+	test a=new test();
+	a.china();
+	a.india();
+	a.japan();
+	
+}
 }
